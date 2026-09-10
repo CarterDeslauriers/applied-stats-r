8256eaf542f6944d6fdb0cb019f584d35f0fe57b
Carter Deslauriers <cdeslauriers928@gmail.com>
2026-09-09 20:45:01 -0400

    Derived logistic regression and gradient decent, I will show it in the next few days or so in a LaTeX file and then implmenet it from scratch and call sklearn if I need to because thats faster for interates and seeded runs and stuff, also I want to have good looking visualiations so I can do that tomorrow or in the next few days

---
+++ IBL_General/Logistic_Regression.py
@@ -0,0 +1,15 @@
+
+# From my current understanding sklearn is logisitic regression the way I derivied it
+# But they use different methods to get there faster
+
+# So write out the gradient decent and the logisitic regression
+# Also create an link a LaTeX file inside of here probably in my notes where I walk through and explain
+
+# Rederive everything in the LaTeX file and leave it in a notes form like my audience is me
+
+# Then write out the function or do it first something like this
+
+def logisitic_regression(whatever, blah, fast=False):
+    # if fast=True just call sklearn
+    # I can read the documentation later if I am interested which I am
+    pass
--- IBL_General/Visualize.py
+++ IBL_General/Visualize.py
@@ -2,12 +2,11 @@ import matplotlib.pyplot as plt
 
 # Create a consistent color scheme
 colors = {
-    "baseline": 
-
+    "baseline": "blue"
 }
 
 # Plot everything
 # SEE Notes/matplotlib for matplotlib notes
 def scatter_plot(results_df, model_name):
-
+    pass
     
